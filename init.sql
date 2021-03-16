39cd4b3509d836df563b80f349eafea18acb601f
andresmauricio <31702026+andresmauricio@users.noreply.github.com>
2021-03-16 10:57:41 -0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,22 @@
 -- Este script no se debe ejecutar, es solo para .github--
 
 
-CREATE DATABASE IF NOT EXISTIS universidad;
+CREATE DATABASE IF NOT EXISTS universidad;
 USE universidad;
+
+CREATE TABLE IF NOT EXISTS test (
+  id_unidad INT AUTO_INCREMENT PRIMARY KEY ,
+  codigo INT NOT NULL,
+  nombre VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS test2 (
+  id_compania INT AUTO_INCREMENT PRIMARY KEY,
+  numero INT NOT NULL,
+  actividad VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS test3 (
+  id_ubicacion INT AUTO_INCREMENT PRIMARY KEY,
+  ciudad VARCHAR(45) NOT NULL
+);
