9b1ad33a4251744f2f877061acf9e4afc0494d8b
andresmauricio <31702026+andresmauricio@users.noreply.github.com>
2021-04-13 13:46:15 -0600

    update correct first triggrer

--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,16 @@
+CREATE TABLE aud_programa (
+    id             VARCHAR2(256) NOT NULL,
+    accion         VARCHAR2(256) NOT NULL,
+    fecha          DATE,
+    usuario        VARCHAR2(256) NOT NULL
+);
+
+
+ALTER TABLE aud_programa ADD CONSTRAINT aud_programa_pk PRIMARY KEY ( id );
+
+
 CREATE OR REPLACE TRIGGER AUDITORIA_PROGRAMA
-    AFTER INSERT ON PROGRAMA
+    AFTER INSERT ON programa
         FOR EACH ROW 
             DECLARE 
                 V_USERNAME VARCHAR(50);
@@ -7,18 +18,8 @@ CREATE OR REPLACE TRIGGER AUDITORIA_PROGRAMA
             BEGIN
                 SELECT USER INTO V_USERNAME FROM DUAL; 
                 SELECT SYSDATE INTO V_FECHA FROM DUAL; 
-                INSERT INTO aud_programa(id, accion, fecha, usuario) VALUES ('ID-' || NEW.id, 'SE HA CREADO UN PROGRAMA' || NEW.titulo, V_FECHA, V_USERNAME );
+                INSERT INTO aud_programa(id, accion, fecha, usuario) VALUES ('ID-' || :NEW.id, 'SE HA CREADO UN PROGRAMA' || :NEW.nombre, V_FECHA, V_USERNAME );
             END;
-
-CREATE TABLE aud_programa (
-    id             VARCHAR2(256) NOT NULL,
-    accion         VARCHAR2(256) NOT NULL,
-    fecha          DATE,
-    b_leausuariorning_id  VARCHAR2(256) NOT NULL
-);
-
-
-ALTER TABLE aud_programa ADD CONSTRAINT aud_programa_pk PRIMARY KEY ( id );
 
 
 INSERT INTO programa   VALUES(5, 'INGENIERIA DE ALIMENTOS');
